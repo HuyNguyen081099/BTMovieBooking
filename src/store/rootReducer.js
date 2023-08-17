2820179dab2cc45bb64038c943a325bcac5232bf
HuyNguyen <huy081099@gmail.com>
2023-08-18 00:32:24 +0700

    upload

---
+++ src/store/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+import { baiTapMovieBookingReducer } from './baiTapMovieBooking/slice'
+
+export const rootReducer = combineReducers({
+
+    baiTapMovieBooking: baiTapMovieBookingReducer,
+})
